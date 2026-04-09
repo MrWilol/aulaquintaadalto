9c2a047376796f252257326578a05c2e3efa0364
85881295072 <85881295072@senacrs.edu.br>
2026-04-09 11:20:50 -0300

    Aula09/04

--- Aula03-apiRest/Front/js/script.js
+++ Aula03-apiRest/Front/js/script.js
@@ -4,22 +4,26 @@ function carregarProdutos(){
     req.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             var produtos = JSON.parse( this.responseText )
-            var txt = '<div class="row">'
-                txt += '  <div class="col">  <strong> Código </strong></div>'
-                txt += '  <div class="col">  <strong> Nome </strong></div>'
-                txt += '  <div class="col"> <strong> Preço </strong></div>'
-                txt += '  <div class="col"> <strong> Categoria </strong></div>'
-                txt += '</div> '
+            var txt = '<thead>'
+                txt = '<tr>'
+                txt += '  <th scope="col">  <strong> Código </strong></th>'
+                txt += '  <th scope="col">  <strong> Nome </strong></th>'
+                txt += '  <th scope="col"> <strong> Preço </strong></th>'
+                txt += '  <th scope="col"> <strong> Categoria </strong></th>'
+                txt += '</tr>'
+                txt += '</thead>'
 
+                txt += '<tbody>'
             produtos.forEach( prod =>{
-                txt +=  '<div class= "row">'
-                txt += '<div class="col"> ' + prod.id + '</div>'
-                txt += '<div class="col"> ' + prod.nome + '</div>'
-                txt += '<div class="col"> ' + prod.preco + '</div>'
-                txt += '<div class="col"> ' + prod.categoria + '</div>'
-                txt += '</div>'
-
+                txt += '<tr>'
+                txt += '<th scope="col"> ' + prod.id + '</th>'
+                txt += '<th scope="col"> ' + prod.nome + '</th>'
+                txt += '<th scope="col"> ' + prod.preco + '</th>'
+                txt += '<th scope="col"> ' + prod.categoria + '</th>'
+                txt += '</tr>'
             })
+
+            txt += '</tbody>'
             document.getElementById("divConteudo").innerHTML = txt
         }
     }
@@ -33,18 +37,22 @@ function carregarCategorias(){
     req.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             var categoria = JSON.parse( this.responseText )
-            var txt = '<div class="row">'
-                txt += '  <div class="col">  <strong> Código </strong></div>'
-                txt += '  <div class="col">  <strong> Categoria </strong></div>'
-                txt += '</div> '
+            var txt = '<thead>'
+            var txt = '<tr>'
+                txt += '  <th scope="col">  <strong> Código </strong></th>'
+                txt += '  <th scope="col">  <strong> Categoria </strong></th>'
+                txt += '</tr> '
+                txt += '</thead> '
 
+                txt +=  '<thbody>'
             categoria.forEach( cat =>{
-                txt +=  '<div class= "row">'
-                txt += '<div class="col"> ' + cat.id + '</div>'
-                txt += '<div class="col"> ' + cat.categoria + '</div>'
-                txt += '</div>'
+                txt +=  '<tr>'
+                txt += '<th scope="col"> ' + cat.id + '</div>'
+                txt += '<th scope="col"> ' + cat.categoria + '</div>'
+                txt +=  '</tr>'
 
             })
+            txt +=  '<tbody>'
             document.getElementById("divConteudo").innerHTML = txt
         }
     }
