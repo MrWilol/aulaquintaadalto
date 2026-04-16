a6eee06c45c6acda06bb2dc65c118877a71a2d70
85881295072 <85881295072@senacrs.edu.br>
2026-04-16 11:51:36 -0300

    aula07

--- Aula03-apiRest/Front/js/script.js
+++ Aula03-apiRest/Front/js/script.js
@@ -35,27 +35,56 @@ function carregarCategorias(){
     var req = new XMLHttpRequest()
 
     req.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            var categoria = JSON.parse( this.responseText )
-            var txt = '<thead>'
-            var txt = '<tr>'
-                txt += '  <th scope="col">  <strong> Código </strong></th>'
-                txt += '  <th scope="col">  <strong> Categoria </strong></th>'
-                txt += '</tr> '
-                txt += '</thead> '
+        if( this.readyState == 4 && this.status == 200){
+            var categorias = JSON.parse( this.responseText )
+            var txt =   '<div class="row"> '
+            txt +=      '   <div class="col"> <strong> Código </strong></div>'
+            txt +=      '   <div class="col"> <strong> Nome </strong></div>'
+            txt +=      '   <div class="col"> <strong> Editar </strong></div>'
+            txt +=      '</div> '
 
-                txt +=  '<thbody>'
-            categoria.forEach( cat =>{
-                txt +=  '<tr>'
-                txt += '<th scope="col"> ' + cat.id + '</div>'
-                txt += '<th scope="col"> ' + cat.categoria + '</div>'
-                txt +=  '</tr>'
-
+            categorias.forEach( cat => {
+                txt +=      '<div class="row"> '
+                txt +=      '   <div class="col"> ' + cat.id + ' </div>'
+                txt +=      '   <div class="col" id="divNome' + cat.id +'"> ' + cat.nome + ' </div>'
+                txt +=      '   <div class="col" id="divBtn'+cat.id+'">'
+                txt +=              '<button class="btn btn-success" onclick="formEditar(' + cat.id +')">Editar</button>' 
+                txt +=      '   </div>'
+                txt +=      '</div> '
             })
-            txt +=  '<tbody>'
             document.getElementById("divConteudo").innerHTML = txt
+                  
         }
     }
     req.open("GET", "http://localhost:8001/product", true)
     req.send()
-}   
+}    
+
+function formEditar( idCat ){
+    var divNome = document.getElementById( "divNome" + idCat )
+    var nome = divNome.innerHTML
+    var novaDivNome = '<input type="text" id="txtNome" value="' + nome +'" > '
+    divNome.innerHTML = novaDivNome
+    var divEditar = document.getElementById("divBtn" + idCat)
+    divEditar.innerHTML = '<button class="btn btn-danger" onclick="editar(' + idCat + ')" >Salvar</button>'
+}
+
+function editar( idCat ){
+    var novoNome = document.getElementById("txtNome").value
+    if( novoNome != "" ){
+        var req = new XMLHttpRequest()
+        req.onreadystatechange = function(){
+            if( this.readyState == 4 ){
+                carregarCategorias()
+            }
+        }
+        req.open("PUT", "http://localhost:8001/category/" + idCat , true)
+
+        //req.setRequestHeader( "Content-type" , "application/x-www-form-urlencoded" );
+        //req.send( "nome=" + novoNome );
+
+        req.setRequestHeader( "Content-type" , "application/json" )
+        req.send( JSON.stringify( { nome : novoNome }) )
+
+    }
+}
